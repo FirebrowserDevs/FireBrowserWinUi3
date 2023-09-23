32ed8193c9a936b1afd9032b27130b7b04908591
jarno9981 <53493418+jarno9981@users.noreply.github.com>
2023-09-23 21:14:40 +0200

    patch 4
    finihd setup for design

--- FireBrowserBusiness/Setup/SetupFinish.xaml.cs
+++ FireBrowserBusiness/Setup/SetupFinish.xaml.cs
@@ -23,9 +23,36 @@ namespace FireBrowserWinUi3.Setup
     /// </summary>
     public sealed partial class SetupFinish : Page
     {
+        private int countdownSeconds = 5;
+        private DispatcherTimer timer;
         public SetupFinish()
         {
             this.InitializeComponent();
+            this.Loaded += SetupFinish_Loaded;
+        }
+
+        private void SetupFinish_Loaded(object sender, RoutedEventArgs e)
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+
+            // Start the timer
+            timer.Start();
+        }
+
+        private async void Timer_Tick(object sender, object e)
+        {
+            countdownSeconds--;
+
+            if (countdownSeconds <= 0)
+            {
+                Microsoft.Windows.AppLifecycle.AppInstance.Restart("");
+            }
+            else
+            {
+               
+            }
         }
     }
 }
