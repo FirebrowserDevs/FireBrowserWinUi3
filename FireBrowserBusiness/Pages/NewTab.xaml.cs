1c21305220c45af0dae886163fc758e6a9e3e454
jarno9981 <53493418+jarno9981@users.noreply.github.com>
2024-02-20 09:02:00 +0100

    merge patch

--- FireBrowserBusiness/Pages/NewTab.xaml.cs
+++ FireBrowserBusiness/Pages/NewTab.xaml.cs
@@ -35,11 +35,7 @@ public sealed partial class NewTab : Page
 
     private void NewTab_Loaded(object sender, RoutedEventArgs e)
     {
-        //NO need to load because property is attached to viewModel, and also if you select the tab it will call the load event may we can refresh the page... 
 
-        //bool isNtp = userSettings.NtpDateTime == "1";
-        //DateTimeToggle.IsOn = isNtp;
-        //NtpEnabled(isNtp);
     }
 
     FireBrowserMultiCore.Settings userSettings = UserFolderManager.LoadUserSettings(AuthService.CurrentUser);
@@ -48,10 +44,6 @@ public sealed partial class NewTab : Page
         Type.IsOn = userSettings.Auto == "1";
         Mode.IsOn = userSettings.LightMode == "1";
 
-        //ViewModel = new HomeViewModel
-        //{
-        //    BackgroundType = GetBackgroundType(userSettings.Background)
-        //};
         ViewModel.BackgroundType = GetBackgroundType(userSettings.Background);
         // set the ntpClock control visibility 
         await ViewModel.Intialize();
@@ -76,45 +68,6 @@ public sealed partial class NewTab : Page
             _ => Settings.NewTabBackground.None
         };
     }
-
-    //private async void NtpEnabled(bool isNtp)
-    //{
-    //    while (isNtp)
-    //    {
-    //        await Task.Delay(100);
-
-    //        if (NtpTime is not null && NtpDate is not null)
-    //        {
-    //            try
-    //            {
-    //                NtpTime.Visibility = NtpDate.Visibility = Visibility.Visible;
-    //                (NtpTime.Text, NtpDate.Text) = (DateTime.Now.ToString("H:mm"), $"{DateTime.Today.DayOfWeek}, {DateTime.Today.ToString("MMMM d")}");
-    //            }
-    //            catch (Exception ex)
-    //            {
-    //                ExceptionLogger.LogException(ex);
-    //                break;
-    //            }
-    //        }
-    //        else
-    //        {
-    //            break;
-    //        }
-    //    }
-
-    //    if (NtpTime is not null && NtpDate is not null)
-    //    {
-    //        try
-    //        {
-    //            NtpTime.Visibility = NtpDate.Visibility = Visibility.Collapsed;
-    //        }
-    //        catch (Exception ex)
-    //        {
-    //            ExceptionLogger.LogException(ex);
-    //        }
-    //    }
-    //}
-
 
     private void SetVisibilityBasedOnLightMode(bool isLightMode)
     {
