a0363b543b9d6e75f907bd67b0ba4413c9a95495
jarno9981 <53493418+jarno9981@users.noreply.github.com>
2024-02-27 11:57:47 +0100

    Delete FireBrowserBusinessCore directory

--- FireBrowserBusinessCore/CoreUi/ConfirmAppClose.xaml.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.UI.Xaml.Controls;
-
-namespace FireBrowserWinUi3Core.CoreUi;
-
-public sealed partial class ConfirmAppClose : ContentDialog
-{
-    public ConfirmAppClose() => InitializeComponent();
-}
