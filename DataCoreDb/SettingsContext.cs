c194d94a2e106ac6de57660679555c307c4a40d7
jarno9981 <53493418+jarno9981@users.noreply.github.com>
2024-02-29 21:04:04 +0100

    small patch

--- DataCoreDb/SettingsContext.cs
+++ DataCoreDb/SettingsContext.cs
@@ -2,25 +2,21 @@
 using Microsoft.EntityFrameworkCore;
 using System.IO;
 
-namespace FireBrowserDataCore
+namespace FireBrowserDataCore;
+
+public class SettingsContext : DbContext
 {
-    public class SettingsContext : DbContext
+    public DbSet<Settings> Settings { get; set; }
+    public string ConnectionPath { get; set; }
+    public SettingsContext(string username)
     {
-        public DbSet<Settings> Settings { get; set; }
-        public string ConnectionPath { get; set; }
-        public SettingsContext(string username)
-        {
-            ConnectionPath = Path.Combine(UserDataManager.CoreFolderPath, UserDataManager.UsersFolderPath, username, "Settings", "Settings.db");
+        ConnectionPath = Path.Combine(UserDataManager.CoreFolderPath, UserDataManager.UsersFolderPath, username, "Settings", "Settings.db");
 
-        }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
 
-            optionsBuilder.UseSqlite($"Data Source={ConnectionPath}");
-
-        }
-
-
+        optionsBuilder.UseSqlite($"Data Source={ConnectionPath}");
 
     }
 }
