0f534662d253933a01b587efee4cbbbc8b66752f
jarno9981 <53493418+jarno9981@users.noreply.github.com>
2024-09-22 11:17:04 +0200

    Revert "patch"
    This reverts commit bf046db98a1865e9a0b812ba29c2d195e333e7c4.

--- FireBrowserWinUi3/Services/AppService.cs
+++ FireBrowserWinUi3/Services/AppService.cs
@@ -187,7 +187,6 @@ public static class AppService
             appWindow.MoveInZOrderAtTop();
             appWindow.TitleBar.ExtendsContentIntoTitleBar = true;
             appWindow.Title = "UserCentral";
-            appWindow.SetIcon("logo.ico");
 
             var titleBar = appWindow.TitleBar;
             var btnColor = Colors.Transparent;
@@ -196,6 +195,7 @@ public static class AppService
             titleBar.ButtonBackgroundColor = btnColor;
             titleBar.ButtonInactiveBackgroundColor = btnColor;
             appWindow.SetPresenter(AppWindowPresenterKind.CompactOverlay);
+            appWindow.SetIcon("ms-appx:///logo.ico");
         }
     }
 
