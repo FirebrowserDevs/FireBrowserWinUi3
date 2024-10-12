e6d70abe0a0835bf113856bef4646e0732f2b86a
jarno9981 <53493418+jarno9981@users.noreply.github.com>
2024-10-12 22:43:04 +0200

    payment gateway
    please do not use yet because the link is not valid its in test mode

--- FireBrowserWinUi3/Controls/PaymentWindow.xaml.cs
+++ FireBrowserWinUi3/Controls/PaymentWindow.xaml.cs
@@ -1,24 +1,59 @@
+using Microsoft.UI.Windowing;
+using Microsoft.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.Web.WebView2.Core;
 using System;
 using System.IO;
+using Windows.Graphics;
+using WinRT.Interop;
 
 namespace FireBrowserWinUi3.Controls
 {
     public sealed partial class PaymentWindow : Window
     {
         private string premiumLicensePath;
-
+        private AppWindow appWindow;
+        private AppWindowTitleBar titleBar;
 
         public PaymentWindow()
         {
             this.InitializeComponent();
             PaymentWebView.EnsureCoreWebView2Async();
             premiumLicensePath = Path.Combine(AppContext.BaseDirectory, "premium.license"); // Path for license file
+            InitializeWindow();
         }
 
-      
+        private void InitializeWindow()
+        {
+            var hWnd = WindowNative.GetWindowHandle(this);
+            WindowId windowId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            appWindow = AppWindow.GetFromWindowId(windowId);
+
+            appWindow.MoveAndResize(new RectInt32(500, 500, 1650, 1000));
+            FireBrowserWinUi3Core.Helpers.Windowing.Center(this);
+            appWindow.SetPresenter(AppWindowPresenterKind.Default);
+            appWindow.MoveInZOrderAtTop();
+            appWindow.ShowOnceWithRequestedStartupState();
+            appWindow.SetIcon("buy.ico");
+            AppWindow.Title = "Buy Premium";
+
+            if (!AppWindowTitleBar.IsCustomizationSupported())
+            {
+                throw new Exception("Unsupported OS version.");
+            }
+            else
+            {
+                titleBar = appWindow.TitleBar;
+                titleBar.ExtendsContentIntoTitleBar = true;
+                var btnColor = Colors.Transparent;
+                titleBar.BackgroundColor = btnColor;
+                titleBar.ButtonBackgroundColor = btnColor;
+                titleBar.InactiveBackgroundColor = btnColor;
+                titleBar.ButtonInactiveBackgroundColor = btnColor;
+            }
+        }
+
         private async void PaymentWebView_NavigationCompleted(WebView2 sender, CoreWebView2NavigationCompletedEventArgs args)
         {
             var currentUrl = sender.Source.ToString();
